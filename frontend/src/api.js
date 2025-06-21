f4d34dc8230f048d64216a901d9126d802eb820c
CatalinGiga <catalin.giga@gmail.com>
2025-06-21 11:57:52 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+//const API_URL = 'http://localhost:8000';
+const API_URL = 'https://mpp-exam-backend-yrwm.onrender.com';
+
 
 const api = axios.create({
   baseURL: API_URL,
